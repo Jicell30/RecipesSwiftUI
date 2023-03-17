c496026664762a22463aa19397078988e0e73c60
Jicell30 <gissellcollado30@gmail.com>
2023-03-17 14:22:48 -0400

    Initial commit

---
+++ RecipesSwiftUI/RecipesSwiftUI/RecipesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesSwiftUIApp.swift
+//  RecipesSwiftUI
+//
+//  Created by Jicell on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
